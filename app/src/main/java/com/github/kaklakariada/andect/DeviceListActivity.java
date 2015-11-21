9304e7f2ade4a1cc9d54a59c22c0ccc00dcc117a
kaklakariada <kaklakariada@users.noreply.github.com>
2015-11-21 18:08:55 +0100

    add master/detail flow

--- app/src/main/java/com/github/kaklakariada/andect/DeviceListActivity.java
+++ app/src/main/java/com/github/kaklakariada/andect/DeviceListActivity.java
@@ -1,106 +1,142 @@
 package com.github.kaklakariada.andect;
 
-import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.widget.SimpleCursorAdapter;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.ProgressBar;
+import android.widget.TextView;
 
-import com.github.kaklakariada.fritzbox.FritzBoxSession;
-import com.github.kaklakariada.fritzbox.HomeAutomation;
-import com.github.kaklakariada.fritzbox.http.HttpTemplate;
-import com.github.kaklakariada.fritzbox.model.homeautomation.Device;
-import com.github.kaklakariada.fritzbox.model.homeautomation.DeviceList;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.github.kaklakariada.andect.dummy.DummyContent;
 
 import java.util.List;
 
-public class DeviceListActivity extends ListActivity {
+/**
+ * An activity representing a list of Devices. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link DeviceDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+public class DeviceListActivity extends AppCompatActivity {
 
-    private static final Logger LOG = LoggerFactory.getLogger(DeviceListActivity.class);
-
-    private FritzBoxSession fritzBoxSession = null;
-    private ArrayAdapter<Device> listAdapter;
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_list);
-        listAdapter = new ArrayAdapter<Device>(this, android.R.layout.simple_list_item_1);
-        setListAdapter(listAdapter);
-        if(fritzBoxSession == null) {
-            Intent intent = new Intent(this, ConnectActivity.class);
-            LOG.info("Starting login...");
-            startActivityForResult(intent, 1);
-            return;
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        View recyclerView = findViewById(R.id.device_list);
+        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
+
+        if (findViewById(R.id.device_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String url = data.getStringExtra("url");
-        String sid = data.getStringExtra("sid");
-        LOG.info("Got url {} and sid {}", url, sid);
-        this.fritzBoxSession = new FritzBoxSession(new HttpTemplate(url), sid);
-        updateDeviceList();
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
     }
 
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        Device selectedDevice = (Device) getListView().getItemAtPosition(position);
-        LOG.info("Device selected: {}", selectedDevice);
-    }
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
 
-    private void updateDeviceList() {
-        LOG.info("Update device list");
-        listAdapter.clear();
-        ProgressBar progressBar = new ProgressBar(this);
-        progressBar.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.WRAP_CONTENT,
-                AbsListView.LayoutParams.WRAP_CONTENT, Gravity.CENTER));
-        progressBar.setIndeterminate(true);
-        getListView().setEmptyView(progressBar);
+        private final List<DummyContent.DummyItem> mValues;
 
-        ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
-        root.addView(progressBar);
-
-        new UpdateDeviceListTask().execute();
-    }
-
-    private class UpdateDeviceListTask extends AsyncTask<Void, Void, DeviceList> {
-
-        @Override
-        protected DeviceList doInBackground(Void... params) {
-            DeviceList deviceList = new HomeAutomation(fritzBoxSession).getDeviceListInfos();
-            LOG.info("Found {} devices, api version: {}", deviceList.getDevices().size(), deviceList.getApiVersion());
-            return deviceList;
+        public SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items) {
+            mValues = items;
         }
 
         @Override
-        protected void onPostExecute(final DeviceList deviceList) {
-            LOG.info("Display device list {}", deviceList);
-            listAdapter.addAll(deviceList.getDevices());
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.device_list_content, parent, false);
+            return new ViewHolder(view);
         }
 
         @Override
-        protected void onCancelled() {
-            LOG.info("Task cancelled");
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+            holder.mIdView.setText(mValues.get(position).id);
+            holder.mContentView.setText(mValues.get(position).content);
+
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mTwoPane) {
+                        Bundle arguments = new Bundle();
+                        arguments.putString(DeviceDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+                        DeviceDetailFragment fragment = new DeviceDetailFragment();
+                        fragment.setArguments(arguments);
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.device_detail_container, fragment)
+                                .commit();
+                    } else {
+                        Context context = v.getContext();
+                        Intent intent = new Intent(context, DeviceDetailActivity.class);
+                        intent.putExtra(DeviceDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+
+                        context.startActivity(intent);
+                    }
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mIdView;
+            public final TextView mContentView;
+            public DummyContent.DummyItem mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mIdView = (TextView) view.findViewById(R.id.id);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mContentView.getText() + "'";
+            }
         }
     }
 }
